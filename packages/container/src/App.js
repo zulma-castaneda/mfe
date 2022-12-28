5f7866974e187d4fe5f3abdfb525eab4c8651414
zulma-castaneda <57265842+zulma-castaneda@users.noreply.github.com>
2022-12-28 11:47:07 -0500

    copied project

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
   return (
     <div>
       <h1>Hi there!</h1>
+      <hr />
       <hr />
       <MarketingApp />
     </div>
